c648ee1746997770795bdef437f88f49050fe1f7
ilim <ilim0t14@gmail.com>
2020-03-07 19:05:10 +0900

    eslintの設定

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:prettier/recommended"
+      ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "prettier/prettier": "error",
+        "no-var": "warn",
+        "no-unused-vars": "warn"
+    }
+};
