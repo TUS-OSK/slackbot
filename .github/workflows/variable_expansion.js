4fc402b34d5f42556d82aaf4765887ea7c55e553
ilim <ilim0t14@gmail.com>
2020-03-09 03:03:46 +0900

    Typo

--- .github/workflows/variable_expansion.js
+++ .github/workflows/variable_expansion.js
@@ -10,7 +10,7 @@ let fileData = fs.readFileSync(fileName, "utf8");
 fileData = fileData.replace(/\${\w+}/g, match => {
   const key = match.slice(2, -1);
   if (!(key in process.env)) {
-    throw new ReferenceError(`${fileName}が未定義の環境変数を参照しています。`);
+    throw new ReferenceError(`${fileName}が未定義の環境変数${key}を参照しています。`);
   }
   return process.env[key];
 });
