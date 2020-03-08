d61b6143ec944ff433736ddb2ab9d22a8f39a406
ilim <ilim0t14@gmail.com>
2020-03-09 02:57:29 +0900

    Deploy Scriptの修正 & gcloudignoreの追加

--- .github/workflows/variable_expansion.js
+++ .github/workflows/variable_expansion.js
@@ -1,13 +1,18 @@
 const fs = require("fs");
 
-let fileData = fs.readFileSync("./app.yaml", "utf8");
+if (process.length < 3) {
+  throw new Error("変数を展開するyamlファイルが指定されていません。");
+}
+const fileName = process.argv[2];
+
+let fileData = fs.readFileSync(fileName, "utf8");
 
 fileData = fileData.replace(/\${\w+}/g, match => {
   const key = match.slice(2, -1);
   if (!(key in process.env)) {
-    throw new ReferenceError("app.ymlが未定義の環境変数を参照しています。");
+    throw new ReferenceError(`${fileName}が未定義の環境変数を参照しています。`);
   }
   return process.env[key];
 });
 
-fs.writeFileSync("./app.yaml", fileData, "utf8");
+fs.writeFileSync(fileName, fileData, "utf8");
