a5ac1542ebe89e9ba45e0f13ff821150290c16b7
ilim <ilim0t14@gmail.com>
2020-03-09 02:38:02 +0900

    deploy scriptのupdate

--- .github/workflows/variable_expansion.js
+++ .github/workflows/variable_expansion.js
@@ -3,7 +3,11 @@ const fs = require("fs");
 let fileData = fs.readFileSync("./app.yaml", "utf8");
 
 fileData = fileData.replace(/\${\w+}/g, match => {
-  return process.env[match.slice(2, -1)];
+  const key = match.slice(2, -1);
+  if (!(key in process.env)) {
+    throw new ReferenceError("app.ymlが未定義の環境変数を参照しています。");
+  }
+  return process.env[key];
 });
 
 fs.writeFileSync("./app.yaml", fileData, "utf8");
