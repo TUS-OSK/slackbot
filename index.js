82307e8e45d9651f64b4cc22b0d696b4beaeef4b
ilim <ilim0t14@gmail.com>
2020-03-10 05:45:12 +0900

    ログレベルの設定

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { App } = require("@slack/bolt");
+const { App, LogLevel } = require("@slack/bolt");
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  logLevel: LogLevel.INFO
 });
 
 const allBots = ["example"];
