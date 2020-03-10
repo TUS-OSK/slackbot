994c32134e76b1b13cc93a838561e2a2c2c72919
ilim <ilim0t14@gmail.com>
2020-03-11 05:46:58 +0900

    Tweak

--- bots/poll/parser.ts
+++ bots/poll/parser.ts
@@ -6,11 +6,11 @@ export function parseArgs(str: string): string[] {
   const regex = /"(.*?)"|'(.*?)'|“(.*?)”|‘(.*?)’/g;
   const buf: string[] = [];
   const encoded = str.replace(regex, (match, p1, p2, p3, p4) => {
-    return "$" + `{${buf.push(p1 ?? p2 ?? p3 ?? p4) - 1}}`;
+    return `\${${buf.push(p1 ?? p2 ?? p3 ?? p4) - 1}}`;
   });
 
   if (/("|“|”|'|‘|’)/.test(encoded)) {
-    throw new Error("無効な引数です。");
+    throw new Error(`無効な引数"${str}"です。`);
   }
 
   const decode = (s: string): string => {
@@ -25,36 +25,36 @@ export function parseArgs(str: string): string[] {
   return squeezed.split(" ").map(value => decode(value));
 }
 
-const myAssert = (actual: string[], expected: string[]): void => {
+const assertJSONEqual = (actual: string[], expected: string[]): void => {
   assert.strictEqual(
     JSON.stringify(actual),
     JSON.stringify(expected),
-    `${actual}が${expected}でない`
+    `${actual}が${expected}と等しくありません。`
   );
 };
 
-myAssert(parseArgs(`"hello" test`), ["hello", "test"]);
-myAssert(parseArgs(` "hello" test`), ["hello", "test"]);
-myAssert(parseArgs(`"hello"  test`), ["hello", "test"]);
-myAssert(parseArgs(`"hello" test `), ["hello", "test"]);
-myAssert(parseArgs(`  "hello"  test  `), ["hello", "test"]);
-myAssert(parseArgs(`'hello' test`), ["hello", "test"]);
-myAssert(parseArgs(`'hello' "test"`), ["hello", "test"]);
-myAssert(parseArgs(`"he'll'o" '"test"'`), [`he'll'o`, `"test"`]);
-myAssert(parseArgs(`'he"ll"o' "'test'"`), [`he"ll"o`, "'test'"]);
-myAssert(parseArgs(`"hello"test`), ["hellotest"]);
-myAssert(parseArgs(`he"llo"test`), ["hellotest"]);
-myAssert(parseArgs(`'hello'test`), ["hellotest"]);
-myAssert(parseArgs(` "he'll'o" 'he"ll"o2'  "he 'll' o" 'he "ll" o2'`), [
+assertJSONEqual(parseArgs(`"hello" test`), ["hello", "test"]);
+assertJSONEqual(parseArgs(` "hello" test`), ["hello", "test"]);
+assertJSONEqual(parseArgs(`"hello"  test`), ["hello", "test"]);
+assertJSONEqual(parseArgs(`"hello" test `), ["hello", "test"]);
+assertJSONEqual(parseArgs(`  "hello"  test  `), ["hello", "test"]);
+assertJSONEqual(parseArgs(`'hello' test`), ["hello", "test"]);
+assertJSONEqual(parseArgs(`'hello' "test"`), ["hello", "test"]);
+assertJSONEqual(parseArgs(`"he'll'o" '"test"'`), [`he'll'o`, `"test"`]);
+assertJSONEqual(parseArgs(`'he"ll"o' "'test'"`), [`he"ll"o`, "'test'"]);
+assertJSONEqual(parseArgs(`"hello"test`), ["hellotest"]);
+assertJSONEqual(parseArgs(`he"llo"test`), ["hellotest"]);
+assertJSONEqual(parseArgs(`'hello'test`), ["hellotest"]);
+assertJSONEqual(parseArgs(` "he'll'o" 'he"ll"o2'  "he 'll' o" 'he "ll" o2'`), [
   "he'll'o",
   `he"ll"o2`,
   `he 'll' o`,
   `he "ll" o2`
 ]);
-myAssert(parseArgs(`'he"ll"o'"he'll'o2"`), [`he"ll"ohe'll'o2`]);
-myAssert(parseArgs(`'he"ll"o' "he'll'o2"`), [`he"ll"o`, `he'll'o2`]);
-myAssert(parseArgs(`"a'b"c"d'e"`), [`a'bcd'e`]);
-myAssert(parseArgs(`"hello""world"`), [`helloworld`]);
+assertJSONEqual(parseArgs(`'he"ll"o'"he'll'o2"`), [`he"ll"ohe'll'o2`]);
+assertJSONEqual(parseArgs(`'he"ll"o' "he'll'o2"`), [`he"ll"o`, `he'll'o2`]);
+assertJSONEqual(parseArgs(`"a'b"c"d'e"`), [`a'bcd'e`]);
+assertJSONEqual(parseArgs(`"hello""world"`), [`helloworld`]);
 
 assert.throws(() => parseArgs(`hello " bad`));
 assert.throws(() => parseArgs(`"hello" " bad`));
